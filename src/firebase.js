0f411beda4ab0e9e8b567deafd450a473dad43b2
MysticalMike60t <caden.finkelstein@cttech.org>
2023-03-30 13:24:48 -0400

    Adding Login

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBabo7XfrbgD_z4qIsf3KWHnPwwqUBM5xI",
-  authDomain: "fir-user-reg-auth.firebaseapp.com",
-  projectId: "fir-user-reg-auth",
-  storageBucket: "fir-user-reg-auth.appspot.com",
-  messagingSenderId: "720211998290",
-  appId: "1:720211998290:web:8291fab2697dbaafc2d18b"
-}
+  apiKey: "AIzaSyAUDLSK7BD24MSeDOYE-rVu-9K6RIDEhWU",
+  authDomain: "fir-authtesting-6c271.firebaseapp.com",
+  projectId: "fir-authtesting-6c271",
+  storageBucket: "fir-authtesting-6c271.appspot.com",
+  messagingSenderId: "190068104335",
+  appId: "1:190068104335:web:b07916f9a4d5fef396a4fa",
+  measurementId: "G-HZWM85QSK4"
+};
 
 // Initialize Firebase and Firebase Authentication
 const app = initializeApp(firebaseConfig)
